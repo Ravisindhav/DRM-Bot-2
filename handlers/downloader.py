e2b4cce402fd2328ef5d2a13c1ec67a2f55fda6b
Ravisindhav <sindhavravi11@gmail.com>
2025-07-09 05:49:58 +0530

    Update downloader.py

--- handlers/downloader.py
+++ handlers/downloader.py
@@ -1,350 +1,120 @@
-import ctypes
-import subprocess
-from subprocess import getoutput
-from handlers.url_scripts import ParseLink
-from main import LOGGER as LOGS
-import asyncio
-import requests
-import aiohttp
-import aiofiles
 import os
-from handlers.tg import TgHandler
+from pyrogram import Client as AFK, idle
+from pyrogram.enums import ChatMemberStatus, ChatMembersFilter
+from pyrogram import enums
+from pyrogram.types import ChatMember
+import asyncio
+import logging
+import tgcrypto
+from pyromod import listen
+import logging
+from tglogging import TelegramLogHandler
 
-cc = 0
+# Config 
+class Config(object):
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7053921138:AAGoQZh2x00uTuufuzqEqDYO7YlWQa7KmF8")
+    API_ID = int(os.environ.get("API_ID",  "27498866"))
+    API_HASH = os.environ.get("API_HASH", "96fbb6ad2e11ab04e83ca09ef3f42455")
+    DOWNLOAD_LOCATION = "./DOWNLOADS"
+    SESSIONS = "./SESSIONS"
 
-EXTRA_LINKS = {
-    "CP_VIMEO_TYPE": ("https://videos.classplusapp.com/", "https://api.edukemy.com/videodetails/", "https://tencdn.classplusapp.com", "https://covod.testbook.com/"),
-    "GUIDELY_LINK": ("https://guidely.prepdesk.in/api/", "https://ibpsguide.prepdesk.in/api/"),
-    # "http://104.199.144.5:1935/vod/",
-    "SET3": ("https://ply-404.herokuapp.com/"),
-    "EDU_PDF": ("https://edukemy-v2-assets.s3.ap-south-1.amazonaws.com/course_content/"),
-    "VISION_PDF": ("http://www.visionias.in/student/pt/video_student/handout", "http://www.visionias.in/student/3.php?"),
-    "TOPRANKER": ("https://live.anytimelearning.in/", "https://onlinetest.sure60.com/"),
-}
+    AUTH_USERS = os.environ.get('AUTH_USERS', '6488911325').split(',')
+    for i in range(len(AUTH_USERS)):
+        AUTH_USERS[i] = int(AUTH_USERS[i])
 
+    GROUPS = os.environ.get('GROUPS', '-1002075880942').split(',')
+    for i in range(len(GROUPS)):
+        GROUPS[i] = int(GROUPS[i])
 
-class get_link_atributes:
-    @staticmethod
-    def get_wxh(ytdlp):
-        try:
-            widthXheight = str(
-                getoutput(f"{ytdlp}  -e --get-filename -R 25")).split("\n")[1].strip()
-            LOGS.info(widthXheight)
-            return widthXheight
-        except Exception as e1:
-            LOGS.info(str(e1))
-            widthXheight = ".N.A"
-            return widthXheight
+    LOG_CH = os.environ.get("LOG_CH", "-1002059340064")
 
-    @staticmethod
-    def get_height_width(link: str, Q: str):
-    url = get_link_atributes().input_url(link=link, Q=Q)
-    YTF = f"bv[height<=?{Q}]+ba/[height<=?{Q}]+ba/[height>=?{Q}]+ba/[height<=?{Q}]/[height>=?{Q}]/b"
-    headers = '--add-header "Referer: https://web.classplusapp.com" --add-header "User-Agent: Mozilla/5.0"'
+# TelegramLogHandler is a custom handler which is inherited from an existing handler. ie, StreamHandler.
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s",
+    datefmt='%d-%b-%y %H:%M:%S',
+    handlers=[
+        TelegramLogHandler(
+            token=Config.BOT_TOKEN, 
+            log_chat_id= Config.LOG_CH, 
+            update_interval=2, 
+            minimum_lines=1, 
+            pending_logs=200000),
+        logging.StreamHandler()
+    ]
+)
 
-    if link.endswith("ankul60"):
-        url = ParseLink.topranker_link(link)
-        if "m3u8" in url:
-            rout = ParseLink.rout(url=link, m3u8=url)
-            os.system(f'curl "{rout}" -c "cooks.txt"')
-            cook = "cooks.txt"
-            YTDLP = f'yt-dlp -i --no-check-certificate {headers} -f "{YTF}" --no-warning "{url}" --cookies "{cook}" -o "%(resolution)s"'
-            wXh = get_link_atributes().get_wxh(YTDLP)
-            return wXh
-        elif "youtu" in url:
-            YTDLP = f'yt-dlp -i --no-check-certificate {headers} -f "{YTF}" --no-warning "{url}" --progress -o "%(resolution)s"'
-            wXh = get_link_atributes().get_wxh(YTDLP)
-            return wXh
-    else:
-        YTDLP = f'yt-dlp -i --no-check-certificate {headers} -f "{YTF}" --no-warning "{url}" --progress --remux-video mp4 -o "%(resolution)s"'
-        wXh = get_link_atributes().get_wxh(YTDLP)
-        return wXh
-
-    @staticmethod
-    def input_url(link: str, Q: str):
-        if link.startswith("https://videos.classplusapp.com/"):
-            if link.split("?")[-1].startswith("auth_key="):
-                url = link
-                return url
-            else:
-                url = ParseLink.classplus_link(link=link)
-                return url
-        elif link.startswith(("https://vod.visionias.in/player/index.php", "https://vod.visionias.in/player_v2/index.php")):
-            url = ParseLink.vision_m3u8_link(link, Q)
-            return url
-        elif link.startswith(("https://covod.testbook.com/")):
-            url = ParseLink.classplus_link(link=link)
-            return url
-        elif link.startswith(("https://tencdn.classplusapp.com")):
-            url = ParseLink.classplus_link(link=link)
-            return url
-        elif link.startswith("http://www.visionias.in/student/videoplayer_v2/?"):
-            url = ParseLink.vision_mpd_link(link)
-            return url
-        elif link.startswith("https://d1d34p8vz63oiq.cloudfront.net/"):
-            url = ParseLink.is_pw(link)
-            return url
-        elif link.startswith("https://d1d34p8vz63oiq.cloudfront.net/"):
-            url = ParseLink.is_pw(link)
-            return url
-        elif "drive" in link:
-            url = ParseLink.is_drive_pdf(url=link)
-            return url
-        elif link.startswith("https://videotest.adda247.com/"):
-            if link.split("/")[3] != "demo":
-                url = f'https://videotest.adda247.com/demo/{link.split("https://videotest.adda247.com/")[1]}'
-                return url
-            else:
-                return link
-        elif not link.startswith("http"):
-            url = ParseLink.cw_url2(link.split("*")[0]) + link.split("*")[1]
-            print(url)
-            return url
-        else:
-            url = link
-            return url
+LOGGER = logging.getLogger(__name__)
+LOGGER.info("live log streaming to telegram.")
 
 
-class Download_Methods:
-    def __init__(self, name: str, url: str, path, Token: str, Quality: str) -> None:
-        self.url = url
-        self.name = name
-        self.Q = Quality
-        self.path = path
-        self.token = Token
-        self.temp_dir = f"{path}/{name}"
+# Store
+class Store(object):
+    CPTOKEN = "eyJhbGciOiJIUzM4NCIsInR5cCI6IkpXVCJ9.eyJpZCI6MzgzNjkyMTIsIm9yZ0lkIjoyNjA1LCJ0eXBlIjoxLCJtb2JpbGUiOiI5MTcwODI3NzQyODkiLCJuYW1lIjoiQWNlIiwiZW1haWwiOm51bGwsImlzRmlyc3RMb2dpbiI6dHJ1ZSwiZGVmYXVsdExhbmd1YWdlIjpudWxsLCJjb3VudHJ5Q29kZSI6IklOIiwiaXNJbnRlcm5hdGlvbmFsIjowLCJpYXQiOjE2NDMyODE4NzcsImV4cCI6MTY0Mzg4NjY3N30.hM33P2ai6ivdzxPPfm01LAd4JWv-vnrSxGXqvCirCSpUfhhofpeqyeHPxtstXwe0"
+    SPROUT_URL = "https://discuss.oliveboard.in/"
+    ADDA_TOKEN = ""
+    THUMB_URL = "https://telegra.ph/file/84870d6d89b893e59c5f0.jpg"
 
-    async def m3u82mp4(self, file):
-        subprocess.run(
-            [
-                "ffmpeg", "-hide_banner", "-loglevel", "error", "-protocol_whitelist", "file,http,https,tcp,tls,crypto",
-                "-i", file, "-c", "copy", "-bsf:a", "aac_adtstoasc", f"{self.temp_dir}.mp4"
-            ]
-        )
-        os.remove(file)
-        if os.path.isfile(f"{self.temp_dir}.mp4"):
-            return f"{self.temp_dir}.mp4"
+# Format
+class Msg(object):
+    START_MSG = "**/pro**"
 
-    def addapdf(self):
-        cookies = {
-            'cp_token': f'{self.token}',
-        }
-        headers = {
-            'Host': 'store.adda247.com',
-            'user-agent': 'Mozilla/5.0 (Linux; Android 11; moto g(40) fusion Build/RRI31.Q1-42-51-8; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/97.0.4692.98 Mobile Safari/537.36',
-            'accept': '*/*',
-            'x-requested-with': 'com.adda247.app',
-            'sec-fetch-site': 'same-origin',
-            'sec-fetch-mode': 'cors',
-            'sec-fetch-dest': 'empty',
-            'referer': 'https://store.adda247.com/build/pdf.worker.js',
-            'accept-language': 'en-US,en;q=0.9',
-        }
-        response = requests.get(
-            f'{self.url}', cookies=cookies, headers=headers)
+    TXT_MSG = "Hey <b>{user},"\
+        "\n\n`I'm Multi-Talented Robot. I Can Download Many Type of Links.`"\
+            "\n\nSend a TXT or HTML file :-</b>"
 
-        with open(f'{self.temp_dir}.pdf', 'wb') as f:
-            f.write(response.content)
-        return f'{self.temp_dir}.pdf'
+    ERROR_MSG = "<b>DL Failed ({no_of_files}) :-</b> "\
+        "\n\n<b>Name: </b>{file_name},\n<b>Link:</b> `{file_link}`\n\n<b>Error:</b> {error}"
 
-    async def aio(self):
-        k = f"{self.temp_dir}.pdf"
-        async with aiohttp.ClientSession() as session:
-            async with session.get(self.url) as resp:
-                if resp.status == 200:
-                    f = await aiofiles.open(k, mode='wb')
-                    await f.write(await resp.read())
-                    await f.close()
-        return k
+    SHOW_MSG = "<b>Downloading :- "\
+        "\n`{file_name}`\n\nLink :- `{file_link}`</b>"
 
-    def cwpdf(self):
-        headers = {
-            'User-Agent': 'Dalvik/2.1.0 (Linux; U; Android 9; Redmi Note 5 Pro MIUI/V11.0.5.0.PEIMIXM)',
-            'Host': 'elearn.crwilladmin.com',
-            'Connection': 'Keep-Alive',
-            # 'Accept-Encoding': 'gzip',
-        }
-        r_pdf = requests.get(self.url, headers=headers)
-        with open(f'{self.temp_dir}.pdf', 'wb') as f:
-            f.write(r_pdf.content)
-        pdf = f'{self.temp_dir}.pdf'
-        if os.path.isfile(pdf):
-            return pdf
+    CMD_MSG_1 = "`{txt}`\n\n**Total Links in File are :-** {no_of_links}\n\n**Send any Index From `[ 1 - {no_of_links} ]` :-**"
+    CMD_MSG_2 = "<b>Uploading :- </b> `{file_name}`"
+    RESTART_MSG = "✅ HI Bhai log\n✅ PATH CLEARED"
 
-    def visionpdf(self):
-        cookies = {
-            'PHPSESSID': self.token,
-        }
-        headers = {
-            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-            'Accept-Language': 'en-US,en;q=0.9',
-            'Connection': 'keep-alive',
-            'Upgrade-Insecure-Requests': '1',
-            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36',
-        }
-        response = requests.get(self.url, cookies=cookies,
-                                headers=headers, verify=False)
-        print(response)
-        with open(f"{self.temp_dir}.pdf", "wb") as f:
-            f.write(response.content)
-        if os.path.isfile(f"{self.temp_dir}.pdf"):
-            return f"{self.temp_dir}.pdf"
+# Prefixes
+prefixes = ["/", "~", "?", "!", "."]
 
-    async def Guidely(self):
-        z = requests.get(self.url).json()['item']['data']['key']
-        mpd = requests.get(self.url).json()['item']['data']['file']
-        print(z, mpd)
-        cmd1 = f'yt-dlp -o "{self.path}/Name.%(ext)s" -f "bestvideo[height<={self.Q}]+bestaudio" --allow-unplayable-format --external-downloader aria2c "{mpd}"'
-        os.system(cmd1)
-        AV = os.listdir(self.path)
-        print(AV)
-        for d in AV:
-            if d.endswith("mp4"):
-                cmd2 = f'mp4decrypt --key 1:{z} {self.path}/{d} "{self.path}/video.mp4"'
-                os.system(cmd2)
-                os.remove(f"{self.path}/{d}")
-            elif d.endswith("m4a"):
-                cmd3 = f'mp4decrypt --key 1:{z} {self.path}/{d} "{self.path}/audio.m4a"'
-                os.system(cmd3)
-                os.remove(f"{self.path}/{d}")
-        cmd4 = f'ffmpeg -i "{self.path}/video.mp4" -i "{self.path}/audio.m4a" -c copy "{self.temp_dir}.mp4"'
-        os.system(cmd4)
-        os.remove(f"{self.path}/video.mp4")
-        os.remove(f"{self.path}/audio.m4a")
-        if os.path.isfile(f"{self.temp_dir}.mp4"):
-            file_name = f"{self.temp_dir}.mp4"
-            return file_name
+# Client
+plugins = dict(root="plugins")
+if __name__ == "__main__":
+    if not os.path.isdir(Config.DOWNLOAD_LOCATION):
+        os.makedirs(Config.DOWNLOAD_LOCATION)
+    if not os.path.isdir(Config.SESSIONS):
+        os.makedirs(Config.SESSIONS)
 
-    def get_drive_link_type(self):
-        try:
-            c_type = requests.get(self.url).headers['Content-Type'].lower()
-            return c_type
-        except Exception as e2:
-            LOGS.error(str(e2))
-            c_type = None
-            return c_type
+    PRO = AFK(
+        "AFK-DL",
+        bot_token=Config.BOT_TOKEN,
+        api_id=Config.API_ID,
+        api_hash=Config.API_HASH,
+        sleep_threshold=120,
+        plugins=plugins,
+        workdir= f"{Config.SESSIONS}/",
+        workers= 2,
+    )
 
-    def dot_ws_link(self):
-        response = requests.get(self.url, stream=True)
-        LOGS.info(response.status_code)
-        with open(f"{self.temp_dir}.html", "wb") as f:
-            f.write(response.content)
-        if os.path.isfile(f"{self.temp_dir}.html"):
-            return f"{self.temp_dir}.html"
+    chat_id = []
+    for i, j in zip(Config.GROUPS, Config.AUTH_USERS):
+        chat_id.append(i)
+        chat_id.append(j)
+    
+    
+    async def main():
+        await PRO.start()
+        # h = await PRO.get_chat_member(chat_id= int(-1002115046888), user_id=6695586027)
+        # print(h)
+        bot_info = await PRO.get_me()
+        LOGGER.info(f"<--- @{bot_info.username} Started --->")
+        
+        for i in chat_id:
+            try:
+                await PRO.send_message(chat_id=i, text="**Bot Started! ♾ /pro **")
+            except Exception as d:
+                print(d)
+                continue
+        await idle()
 
-
-class download_handler(Download_Methods):
-    def run_cmd(self, cmd):
-        LOGS.info(cmd)
-        try:
-            dl = subprocess.run(cmd, shell=True)
-        except Exception as e_:
-            LOGS.info(str(e_))
-        file_path = f"{self.temp_dir}.mp4"
-        return file_path
-
-    def recursive(self, cmd):
-        LOGS.info(cmd)
-        global cc
-        if cc >= 5:
-            return
-        dl = subprocess.run(cmd, shell=True)
-        if dl.returncode != 0:
-            cc += 1
-            download_handler.recursive(self, cmd=cmd)
-        cc = 0
-        file_path = f"{self.temp_dir}.mp4"
-        LOGS.info(str(file_path))
-        return file_path
-
-    async def recursive_asyno(self, cmd):
-        LOGS.info(cmd)
-        global cc
-        if cc >= 5:
-            return
-        # Create subprocess
-        process = await asyncio.create_subprocess_shell(
-            cmd=cmd, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
-        )
-        # status
-        print("Started:", cmd, "(pid = " + str(process.pid) + ")", flush=True)
-        # Wait for the subprocess to finish
-        stdout, stderr = await process.communicate()
-        if process.returncode != 0:
-            cc += 1
-            await download_handler.recursive_asyno(self, cmd=cmd)
-        cc = 0
-        # Progress
-        if process.returncode == 0:
-            print("Done:", cmd, "(pid = " + str(process.pid) + ")", flush=True)
-        else:
-            print(
-                "Failed:", cmd, "(pid = " + str(process.pid) + ")", flush=True
-            )
-        file_path = f"{self.temp_dir}.mp4"
-        LOGS.info(str(file_path))
-        return file_path
-
-    async def start_download(self):
-        headers = '--add-header "Referer: https://web.classplusapp.com" --add-header "User-Agent: Mozilla/5.0"'
-YTDLP = f'yt-dlp -i --no-check-certificate -f "{YTF}" --no-warning --add-header "User-Agent: Mozilla/5.0" --add-header "Referer: {self.url}" "{self.url}" --merge-output-format mp4 --remux-video mp4 -o "{self.temp_dir}.%(ext)s"'
-CMD = f"{YTDLP} -R 25 --fragment-retries 25 --external-downloader aria2c --downloader-args 'aria2c: -x 16 -j 32'"
-        # dl = subprocess.run(download_cmd, shell=True)
-
-        if self.url.startswith('https://elearn.crwilladmin.com/') and self.url.endswith('.pdf'):
-            file_name = download_handler.cwpdf(self)
-            return file_name
-
-        if self.url.endswith(".pdf") or ".pdf" in self.url or self.url.startswith(EXTRA_LINKS['EDU_PDF']):
-            file_name = await download_handler.aio(self)
-            return file_name
-
-        if self.url.startswith("https://store.adda247.com/"):
-            file_name = download_handler.addapdf(self)
-            return file_name
-        if self.url.startswith(EXTRA_LINKS['VISION_PDF']):
-            file_name = download_handler.visionpdf(self)
-            return file_name
-
-        if self.url.startswith(EXTRA_LINKS['GUIDELY_LINK']):
-            file_name = await download_handler.Guidely(self)
-            return file_name
-
-        if self.url.startswith("https://videos.sproutvideo.com/"):
-            file = ParseLink.olive(self.Q, self.url, self.path)
-            file_name = await download_handler.m3u82mp4(self, file)
-            return file_name
-        if "drive" in self.url:
-            c_type = download_handler.get_drive_link_type(self)
-            if "pdf" in c_type:
-                file_name = await download_handler.aio(self)
-                return file_name
-            if "video" in c_type:
-                file_name = await download_handler.recursive_asyno(self, cmd=CMD)
-                return file_name
-            else:
-                file_name = await download_handler.aio(self)
-                return file_name
-
-        if self.url.endswith("ankul60"):
-            m3u8url = ParseLink.topranker_link(self.url)
-            if "m3u8" in m3u8url:
-                rout = ParseLink.rout(url=self.url, m3u8=m3u8url)
-                os.system(f'curl "{rout}" -c "cooks.txt"')
-                cook = "cooks.txt"
-                YTDLP = f'yt-dlp -i --no-check-certificate -f "{YTF}" --no-warning "{m3u8url}" --cookies "{cook}" --remux-video mp4 -o "{self.temp_dir}.%(ext)s"'
-                CMD = f"{YTDLP} -R 25 --fragment-retries 25 --external-downloader aria2c --downloader-args 'aria2c: -x 16 -j 32'"
-                file_name = await download_handler.recursive_asyno(self, cmd=CMD)
-                os.remove(cook)
-                return file_name
-            elif "youtu" in m3u8url:
-                self.url = m3u8url
-                file_name = await download_handler.recursive_asyno(self, cmd=CMD)
-                return file_name
-        if self.url.endswith(".ws"):
-            file_name = download_handler.dot_ws_link(self)
-            return file_name
-
-        else:
-            file_name = download_handler.recursive(self, cmd=CMD)
-            return file_name
+    asyncio.get_event_loop().run_until_complete(main())
+    LOGGER.info(f"<---Bot Stopped--->")
